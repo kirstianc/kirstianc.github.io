8c80027a447d05b141e3083f93bccd8aca2b33b0
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-07-08 22:33:13 -0700

    Beginning of React portion of website

---
+++ personal-website/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Ian Chavez</h1>
+      <nav>
+        <ul>
+		<li><h3><a href="https://github.com/kirstianc/ultra-jump-mania">ULTRA JUMP MANIA</a></h3></li>
+	      <p>Contains C# Unity Code and Scripting for CS108 at San Jose State University. Artwork done by Thomas Nazaradeh.</p>	  
+	   	<li><h3><a href="https://github.com/kirstianc/SJSU_CS151_SPRING2021">SJSU CS151 REPOSITORY</a></h3></li>
+	      <p>Contains Java code for object-oriented design course, CS151, at San Jose State University.</p>
+		<li><h3><a href="https://github.com/kirstianc/SIMPLE-COUNTER">BASIC COUNTER</a></h3></li>
+		  <p>Contains Java code to create a counter with increment/decrement capabilites.</p>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
