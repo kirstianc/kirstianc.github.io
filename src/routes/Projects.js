511818287f847352c8ac986064c7a2d801e24b3d
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-08-06 11:29:49 -0700

    rename conventions

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ButtonList from '../components/ButtonList';
-import projBlankImage from '../assets/images/gray.png';
-import proj1Image from '../assets/images/project_images/fillboard.png';
-import proj2Image from '../assets/images/project_images/ultra_jump_mania.png';
+import BlankImage from '../assets/images/gray.png';
+import FillboardImage from '../assets/images/project_images/Fillboard.png';
+import UJMImage from '../assets/images/project_images/UJM.png';
+import DynamicWikiImage from '../assets/images/project_images/DynamicWiki.png';
 
 const Projects = () => {
   const items = [
@@ -10,31 +11,31 @@ const Projects = () => {
       title: 'DynamicWiki',
       description: 'Wiki based on YOUR current episode.',
       link: '/DynamicWiki',
-      image: projBlankImage,
+      image: DynamicWikiImage,
     },
     {
       title: 'FillBoard',
       description: 'Online Party System for Real World benefits.',
       link: 'https://github.com/kirstianc/FillBoard',
-      image: proj1Image,
+      image: FillboardImage,
     },
     {
       title: 'ULTRA JUMP MANIA',
       description: 'Simple 2D platformer created inside the Unity engine.',
       link: 'https://github.com/kirstianc/ultra-jump-mania',
-      image: proj2Image,
+      image: UJMImage,
     },
     {
       title: 'SJSU CS151 REPOSITORY',
       description: 'Object-oriented design course at SJSU.',
       link: 'https://github.com/kirstianc/SJSU_CS151_SPRING2021',
-      image: projBlankImage,
+      image: BlankImage,
     },
     {
       title: 'BASIC COUNTER',
       description: 'Counter with increment/decrement capabilities.',
       link: 'https://github.com/kirstianc/SIMPLE-COUNTER',
-      image: projBlankImage,
+      image: BlankImage,
     },
   ];
 
